9d0f201cd063ada990110b4a7d997a6068fcd243
Noer Fazri Ramadhan <fazri121014@gmail.com>
2020-02-15 21:00:59 +0700

    button login

--- app/src/main/java/com/example/muslimhotel/ui/LoginFragment.java
+++ app/src/main/java/com/example/muslimhotel/ui/LoginFragment.java
@@ -1,27 +1,39 @@
 package com.example.muslimhotel.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.muslimhotel.MainActivity;
 import com.example.muslimhotel.R;
-
-
-
 
 public class LoginFragment extends Fragment {
 
+    private TextView btnLogin;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_login, container, false);
+
+        btnLogin = (TextView) v.findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), HomeActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
 
         return v;
     }
